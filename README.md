40ddcc30d176cb4d5e3b504763a87ae521930436
annescharf <ascharf@ab.mpg.de>
2024-10-16 13:34:03 +0200

    some improvement suggetions

--- README.md
+++ README.md
@@ -28,5 +28,7 @@ The app models steps *observed* from collected location fixes with the *generate
 
 ### Settings 
 
-*Example:* `Type`: Specify how you want to perform the step selection analysis. Type population will generate the random steps based on step-length and turn-angle distribution of all the individuals and type individual will generate random steps based on step-length and turn-angle distribution separately for all the individuals, default is `population`.
+`Raster files`: You can upload your own raster file for running the resource selection function analysis. Make sure that they overlap with the tracking data set and are in WGS 84 projection! You can only upload one file, but this one .tif file can contain multiple layers. Note that if you do not upload a file, a fallback raster file of distance to roads in Y2Y region is provided.
+
+`Type`: Specify how you want to perform the step selection analysis. Type `population` will generate the random steps based on step-length and turn-angle distribution of all the individuals and type `individual` will generate random steps based on step-length and turn-angle distribution separately for all the individuals, default is `population`.
 
--- RFunction.R
+++ RFunction.R
@@ -59,15 +59,24 @@ rFunction = function(data, type = "indv", ...) {
     unnest(cols = step_len) %>% 
     ggplot(aes(step_len, fill = factor(id))) + 
     geom_density(alpha = 0.4)+facet_wrap(~id, scales="free")+
-    labs(x= "Step-length (mtr)", fill = "Individual\nId")+
-    theme_bw() +xlim(0,50000)  
+    labs(x= "Step-length (mtr)", fill = "Individual Id")+
+    theme_bw() +xlim(0,50000)+
+    theme(legend.text = element_text(size = 4),
+          legend.key.size=unit(.3,"cm"))+
+    guides(fill=guide_legend(ncol =1))
+    
+  
   
   taplot <- ssfdat %>% dplyr::filter(case_ ==TRUE) %>%
     dplyr::select(id, ta_) %>%  unnest(cols = ta_) %>% 
     ggplot(aes(ta_, fill = factor(id))) + 
     geom_density(alpha = 0.4)+facet_wrap(~id, scales="free")+
-    labs(x= "Turn angle (radian)", fill = "Individual\nId")+
-    theme_bw()  
+    labs(x= "Turn angle (radian)", fill = "Individual Id")+
+    theme_bw()+
+    theme(legend.text = element_text(size = 4),
+          legend.key.size=unit(.3,"cm"))+
+    guides(fill=guide_legend(ncol =1))
+  
   
   ### regression using clogit from the survival package 
   ssfreg <-fit_issf(case_ ~ step_len + log_sl_ + cos_ta_ + raster + strata(step_id), data = ssfdat)
@@ -125,7 +134,10 @@ rFunction = function(data, type = "indv", ...) {
                          ymax = coef +1.96*`se(coef)`),
                      position = position_dodge(width = 0.15))+
       labs(x= "Variables", y= "Coefficient_estimate", col ="Individual_id")+
-      theme_bw()
+      theme_bw()+
+      theme(legend.text = element_text(size = 4),
+            legend.key.size=unit(.3,"cm"))+
+      guides(color=guide_legend(ncol =1))
    }
   
   ## Save the output in csv and jpeg 
